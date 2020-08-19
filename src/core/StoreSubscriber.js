deefc7aaad954eb4283d7301fae7cb15087356c3
Roman <romaniv788@gmail.com>
2020-08-19 20:07:31 +0300

    Routing implemented

--- src/core/StoreSubscriber.js
+++ src/core/StoreSubscriber.js
@@ -11,18 +11,21 @@ export class StoreSubscriber {
         this.prevState = this.store.getState();
 
         this.sub = this.store.subscribe(state => {
-        Object.keys(state).forEach(key => {
-            if (!isEqual(this.prevState[key], state[key])) {
-            components.forEach(component => {
-                if (component.isWatching(key)) {
-                const changes = {[key]: state[key]};
-                component.storeChanged(changes);
+            Object.keys(state).forEach(key => {
+                if (!isEqual(this.prevState[key], state[key])) {
+                components.forEach(component => {
+                    if (component.isWatching(key)) {
+                    const changes = {[key]: state[key]};
+                    component.storeChanged(changes);
+                    }
+                });
                 }
             });
-            }
-        });
 
-        this.prevState = this.store.getState();
+            this.prevState = this.store.getState();
+            if (process.env.NODE_ENV === 'development') {
+                window['redux'] = this.prevState;
+            }
         });
     }
 
